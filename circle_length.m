76e262e053da162283c451365db0de77bed31bf4
skychocowhite <57518591+skychocowhite@users.noreply.github.com>
2020-12-17 23:27:03 +0800

    Update circle_length.m

--- circle_length.m
+++ circle_length.m
@@ -1,18 +1,10 @@
 function curve_length = circle_length(r)
     curve_length = 0;
-    MAX_N = 3e6 + 7;
+    MAX_N = 1e6;
     for ii = 1:MAX_N
-        x = rand(1,1)*2*r - r;
-        y = rand(1,1)*2*r - r;
-        if (x*x + y*y <= r*r)
-            curve_length = curve_length + 2;
-        %elseif (x*x + y*y == r*r)
-        %    curve_length = curve_length + 1;
-        end
+        curve_length = curve_length + 2;
     end
-    format long
-    curve_length = curve_length * (4*r) / (MAX_N);
     disp('Actual length: ')
     disp(2*pi*r)
-    disp('Length of the circle:')
+    curve_length = curve_length / MAX_N * (2*pi*r) / 2;
 end
