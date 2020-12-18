d055d6fc4b3466b80043e44a1a9029e4ba8acb30
skychocowhite <57518591+skychocowhite@users.noreply.github.com>
2020-12-18 14:21:21 +0800

    Update circle_length._monte.m

--- circle_length._monte.m
+++ circle_length._monte.m
@@ -1,15 +1,17 @@
-function curve_length = circle_length_monte(r)
+function curve_length = Circle_length_Monte(r)
     curve_length = 0;
-    MAX_N = 3e6 + 7;
+    MAX_N = 5e6;
     for ii = 1:MAX_N
-        x = rand(1,1)*2*r - r;
-        y = rand(1,1)*2*r - r;
-        if (x*x + y*y <= r*r)
+        theta = rand(1)*(2*pi);
+        p = rand(1)*(2*r);
+        if (p < r)
             curve_length = curve_length + 2;
+        elseif (p == r)
+            curve_length = curve_length + 1;
         end
     end
     format long
-    curve_length = curve_length * (4*r) / (MAX_N);
+    curve_length = curve_length / MAX_N * (4*pi*r) / 2;
     disp('Actual length: ')
     disp(2*pi*r)
     disp('Length of the circle:')
